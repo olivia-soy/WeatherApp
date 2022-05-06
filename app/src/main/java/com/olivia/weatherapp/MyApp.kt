3a19fcb2680bdf105985839dccd08f49117ea499
leesoyoung <soylee0314@gmail.com>
2022-05-06 22:59:28 +0900

    feat: Apply hilt app

---
+++ app/src/main/java/com/olivia/weatherapp/MyApp.kt
@@ -0,0 +1,15 @@
+package com.olivia.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * WeatherApp
+ * my app
+ * @author leesoyoung
+ * @since 2022/05/06
+ */
+@HiltAndroidApp
+class MyApp : Application() {
+
+}
