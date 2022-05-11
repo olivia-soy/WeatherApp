a650674a7aa7d74b8c3ecfa8ae3a2947579596fd
leesoyoung <soylee0314@gmail.com>
2022-05-11 14:47:52 +0900

    refactor: Apply flow

--- app/src/main/java/com/olivia/weatherapp/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/olivia/weatherapp/domain/repository/WeatherRepository.kt
@@ -1,6 +1,8 @@
 package com.olivia.weatherapp.domain.repository
 
 import com.olivia.weatherapp.domain.model.LocationModel
+import kotlinx.coroutines.flow.Flow
+import com.olivia.weatherapp.domain.Result
 
 /**
  * # WeatherApp
@@ -9,6 +11,6 @@ import com.olivia.weatherapp.domain.model.LocationModel
  * @since 2022-05-03
  */
 interface WeatherRepository {
-    suspend fun requestLocationSearch(query: String): List<Int>
+    fun requestLocationSearch(query: String): Flow<Result<List<Int>>>
     suspend fun requestLocation(woeid: Int): LocationModel
 }
